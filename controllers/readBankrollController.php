62e9b53ce5f347f5fd22b43cb40671509e4ee316
dethoreythomas <43248404+thomasdethorey@users.noreply.github.com>
2019-03-05 13:44:36 +0100

    Delete readBankrollController.php

--- controllers/readBankrollController.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-//On remplit la session avec les attributs de l'objet issus de l'hydratation
-$_SESSION['id'] = $user->id;
-$_SESSION['pseudo'] = $user->pseudo;
-$_SESSION['bankroll'] = $user->bankroll;
-
-
- ?>
